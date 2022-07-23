f8fb4b03123e3bd0a3a41d5182a82cb098e2aaa0
ripon_aiub@yahoo.com <mdabdullahalkamran.ripon@dataport.de>
2022-07-23 01:06:14 -0700

    initial commit

---
+++ Documents/Tutorial/LearnC-Sharp/cSharpFundamental/_01HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _01HelloWorld // namespace of the application
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("First Application");
+            Console.ReadLine();
+        }
+    }
+}
